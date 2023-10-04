e2b73191bb9c1ea14a250a0905b439b0d74d2a81
Morion <91880035+Mohammed-Ismail1@users.noreply.github.com>
2023-10-04 14:54:44 +0200

    App basics

--- my-app/src/App/index.jsx
+++ my-app/src/App/index.jsx
@@ -17,18 +17,21 @@ export default class Documentation_chat extends Component{
     render(){
         return(
             <>
-            <form action="POST">
-                <div className='InputContainer'>
-                    
-                        <input type="text" className='InputBox'/>
+            
+            <div className='ContentBar'> {/*ContentBar for all the Headers of the documentation*/} 
+                <div className='Title'>Chat content</div> {/*Title of the bar*/}
+            </div> {/*close the ContentBar div*/} 
+            
+            <form action="POST"> {/*use the <form> to be able to POST the user input*/}
+                <div className='InputContainer'> {/*add a container to hold the elements for the user input bar*/}
 
-                        <input type="file" className="imageInput" id="imageInput" accept="image/*" />
+                        <input type="text" className='InputBox' placeholder='Document the application'/> {/*use the <form> to be able to POST the user input*/}
+                        <input type="file" className="imageInput" id="imageInput" accept="image/*"/> {/*add a image upload button*/}
+                        <label for="imageInput" className="imageInput-label"></label> {/*add a label to the imageInput to make the button an image through css*/}
+                        <input type="Submit" className='SubmitBtn' value=''/> {/*add a submit button to submit the user input*/}
 
-                        <label for="imageInput" className="imageInput-label"></label>
-
-                        <input type="Submit" className='SubmitBtn' value=''/>
-                </div>
-            </form>
+                </div> {/*close the InputContainer div*/}
+            </form> {/*close the form*/}
 
             </>
         )
